8b5ddf1738bcd12f4873b624b4e57ea98fda81f2
Guillaume Barillet <cm@novastone.local>
2018-06-06 11:38:36 +0200

    Login dev

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
-from rest_framework import APIView
-from rest_framework import Response
-from rest_framework import authentication, permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+#from rest_framework import authentication, permissions
 from django.contrib.auth.models import User
 
 class ListUsers(APIView):
@@ -10,8 +10,8 @@ class ListUsers(APIView):
     * Requires token authentication.
     * Only admin users are able to access this view.
     """
-    authentication_classes = (authentication.TokenAuthentication,)
-    permission_classes = (permissions.IsAdminUser,)
+    #authentication_classes = (authentication.TokenAuthentication,)
+    #permission_classes = (permissions.IsAdminUser,)
 
     def get(self, request, format=None):
         """
