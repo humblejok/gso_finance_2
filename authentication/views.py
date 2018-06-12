bdad5608fd335dd78b7420257a3e00b2f85691ac
Guillaume Barillet <cm@novastone.local>
2018-06-12 15:09:31 +0200

    Login dev

--- authentication/views.py
+++ authentication/views.py
@@ -2,27 +2,20 @@ from rest_framework.views import View
 from rest_framework.response import Response
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
 
 @method_decorator(csrf_exempt, name='dispatch')
 class UserLogin(View):
-    response = HttpResponse()
+    response = Response()
 
     def post(self, request):
-        print(request)
         user = json.loads(request.body.decode('utf-8'))
-        print(user)
         user = authenticate(request, username=user['username'], password=user['password'])
         if user is not None:
-            #login(request, user)
-            # Redirect to a success page.
-            print('y')
-            return self.response
+            login(request, user)
+            self.response['feedback'] = 'success'
         else:
-            # Return an 'invalid login' error message.
-            print('n')
-            return self.response
+            self.response['feedback'] = 'failure'
+        return self.response
