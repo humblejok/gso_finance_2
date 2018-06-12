2910949cbe9a14cdf376d1f303d10f386f57ca2e
Guillaume Barillet <cm@novastone.local>
2018-06-12 18:12:12 +0200

    Login dev

--- authentication/views.py
+++ authentication/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
-from django.shortcuts import redirect
 import json
 
 @method_decorator(csrf_exempt, name='dispatch')
