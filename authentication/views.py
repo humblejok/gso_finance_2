7ac7680fb882b933ad22148ab6c73f4631edab9a
Guillaume Barillet <cm@novastone.local>
2018-06-12 15:47:58 +0200

    Login dev

--- authentication/views.py
+++ authentication/views.py
@@ -8,12 +8,15 @@ import json
 
 @method_decorator(csrf_exempt, name='dispatch')
 class UserLogin(View):
+    response = HttpResponse()
 
     def post(self, request):
         user = json.loads(request.body.decode('utf-8'))
         user = authenticate(request, username=user['username'], password=user['password'])
         if user is not None:
             login(request, user)
-            return HttpResponse('success')
+            self.response['feedback'] = 'success'
         else:
-            return HttpResponse('failure')
+            self.response['feedback'] = 'failure'
+        request.body = json.loads({'val' : self.response['feedback']})
+        return self.response
