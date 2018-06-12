021968f68c7242603cf2f804a5f065133bdc0f0e
Guillaume Barillet <cm@novastone.local>
2018-06-12 10:26:23 +0200

    Login dev

--- authentication/views.py
+++ authentication/views.py
@@ -1,17 +1,33 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
+from rest_framework.views import View, UserForm
 from rest_framework.response import Response
-from rest_framework import status
-#from rest_framework import authentication, permissions
+from rest_framework import authentication, permissions, status
 from django.contrib.auth.models import User
 
-class ListUsers(APIView):
-    #authentication_classes = (authentication.TokenAuthentication,)
-    #permission_classes = (permissions.IsAdminUser,)
+class UserFormView(View):
+    form_class = UserForm
+    template_name = 'authentication/registration_form.html'
 
     def get(self, request):
-        usernames = [user.username for user in User.objects.all()]
-        return Response(usernames)
+        form = self.form_class(None)
+        return self.render(request, self.template_name, {'form': form})
 
-    def post(self):
-        pass
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user.set_password(password)
+            user.save()
+
+            user = self.authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    self.login(request, user)
+                    return self.redirect('index')
+
+        return self.render(request, self.template_name, {'form': form})
