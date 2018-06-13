c86f6597d758dee52ea62bca2e12ba51321f91e8
Guillaume Barillet <cm@novastone.local>
2018-06-13 11:38:45 +0200

    Login dev

--- authentication/views.py
+++ authentication/views.py
@@ -1,13 +1,18 @@
 from rest_framework.views import View
 from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
+
+from rest_framework.views import APIView
+from rest_framework import Response, UserSerializer
+
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 import json
 
 @method_decorator(csrf_exempt, name='dispatch')
-class UserLogin(View):
+#class UserLogin(View):
+class UserLogin(APIView):
 
     def post(self, request):
         if request.user.is_authenticated:
@@ -20,4 +25,19 @@ class UserLogin(View):
                 print('User Logged In')
             else:
                 print('Invalid Credentials')
-        return HttpResponse(status=200)
+        #return HttpResponse(status=200)
+        return Response(UserSerializer(request.user).data)
+
+
+'''
+class AuthView(APIView):
+    authentication_classes = (QuietBasicAuthentication,)
+ 
+    def post(self, request, *args, **kwargs):
+        login(request, request.user)
+        return Response(UserSerializer(request.user).data)
+ 
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response({})
+'''
