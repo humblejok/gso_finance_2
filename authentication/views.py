0b4812624f3c224dae2753d5e9b7b9226d089205
gba <gba@visualcapitalmarkets.com>
2018-06-19 14:41:37 +0200

    Login dev

--- authentication/views.py
+++ authentication/views.py
@@ -26,7 +26,7 @@ class UserLogin(View):
                 request, username=user['username'], password=user['password'])
             if auth_user is not None:
                 login(request, auth_user)
-                serializer = self.serializer_class(auth_user)
+                serializer = login.serializer_class(auth_user)
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return HttpResponse(status=200)
 
