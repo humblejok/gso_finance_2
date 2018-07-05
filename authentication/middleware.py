66bcfd8f14b833486b2207a2146355ffb84d0cef
gba <gba@visualcapitalmarkets.com>
2018-07-05 12:13:26 +0200

    middleware

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -16,7 +16,7 @@ class DataObfuscationMiddleware:
     def process_template_response(self, request, response):
         #pprint(dir(response))
         if hasattr(response, 'content_type'):
-            pprint(response.data.OrderedDict)
+            pprint(response.data[0])
         else:
             pprint('bar')
         return response
