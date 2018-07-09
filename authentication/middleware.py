993dc582d45d8683a8dc4fff6806fee741575731
gba <gba@visualcapitalmarkets.com>
2018-07-09 15:00:20 +0200

    getChildItem

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -16,7 +16,7 @@ class DataObfuscationMiddleware:
         if hasattr(response, 'content_type'):
             OCIPH = ObfuscationCipher()
             for index, item in enumerate(response.data):
-                print(str(index) + ' -> ' + item + ' [value: ' + response.data[item] + ']')
+                print(str(index) + ' -> ' + str(item) + ' [value: ' + str(response.data[item]) + ']')
                 if item == 'access':
                     response.data[item] = OCIPH.cipher_controller(response.data[item])
         return response
