2c2e0d12569da77f61c6c3dbf715163dda9303c4
gba <gba@visualcapitalmarkets.com>
2018-07-10 12:32:55 +0200

    decode

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -13,8 +13,7 @@ class DataObfuscationMiddleware:
 
     def process_template_response(self, request, response):
         if hasattr(response, 'data'):
-            #self.getChildItem(response.data)
-            self.codePortName(response.data)
+            self.getChildItem(response.data)
         return response
 
     def getChildItem(self, collection):
@@ -22,17 +21,6 @@ class DataObfuscationMiddleware:
             searchOrdDict = re.search( r'OrderedDict', str(item))
             if searchOrdDict:
                 self.getChildItem(item)
-            elif item == 'access':                    
-                print(collection[item])
-            else :
-                collection[item] = self.OCIPH.cipher_controller(collection[item])
-        return
-
-    def codePortName(self, collection):
-        for index, item in enumerate(collection):
-            searchOrdDict = re.search( r'OrderedDict', str(item))
-            if searchOrdDict:
-                self.codePortName(item)
             elif item == 'default_name':
                 collection[item] = self.OCIPH.cipher_controller(collection[item])
         return
