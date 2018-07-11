41a2fe95cff820248550d2410c8f0ce26949ba2e
gba <gba@visualcapitalmarkets.com>
2018-07-11 17:06:12 +0200

    encode

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -29,25 +29,6 @@ class DataObfuscationMiddleware:
         elif type(var) is dict or type(var) is collections.OrderedDict:
             for key, value in enumerate(var):
                 self.getChildItem(var[value])
-        elif var != 'access' and var != 'refresh':
-            #collection[item] = self.OCIPH.cipher_controller(collection[item])
-            print(var)
         else:
-            print('watwat')
+            print(var)
         return
-        '''
-        for index, item in enumerate(collection):
-            if type(item) is collections.OrderedDict:
-                for i_index, i_item in enumerate(item):
-                    if type(item[i_item]) is collections.OrderedDict:
-                        self.getChildItem(item[i_item])
-                    else:
-                        pass
-                        # item[i_item] = self.OCIPH.cipher_controller(item[i_item])
-            elif item != 'access' and item != 'refresh':
-                print(type(collection[item]))
-                # collection[item] = self.OCIPH.cipher_controller(collection[item])
-            else:
-                print('watwat')
-        return
-        '''
