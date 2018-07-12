ba5c07ed4f193b81703c1b31109bcf588a038df4
gba <gba@visualcapitalmarkets.com>
2018-07-12 10:13:21 +0200

    encode

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -17,7 +17,8 @@ class DataObfuscationMiddleware:
     def process_template_response(self, request, response):
         if hasattr(response, 'data'):
             #self.getChildItem(response.data)
-            response.data = self.OCIPH.cipher_controller(response.data)
+            if not hasattr(response.data, 'access')
+                response.data = self.OCIPH.cipher_controller(response.data)
         return response
 
     def getChildItem(self, var):
