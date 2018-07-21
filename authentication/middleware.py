82822153cbac8d4518b4de2b3a9eaae146e9ba85
Guillaume Barillet <g.barillet@gmail.com>
2018-07-21 19:01:30 +0200

    response

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -25,18 +25,11 @@ class DataObfuscationMiddleware:
         elif hasattr(response, 'data'):
             for i in range(0, len(response.data)):
                 if 'inception_date' in response.data[i]:
-                    print(response.data[i]['inception_date'])
-                else:
-                    print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH')
-                #if hasattr( response.data[i], 'inception_date'):
-                    #print(response.data[i].inception_date)
-                    #response.data[i].inception_date = self.OCIPH.cipher_controller(response.data[i].inception_date)
-                #if hasattr( response.data[i], 'current_aum_local'):
-                    #print(response.data[i].current_aum_local)
-                    #response.data[i].current_aum_local = self.OCIPH.cipher_controller(response.data[i].current_aum_local)
-                #if hasattr( response.data[i], 'current_aum_mgmt'):
-                    #print(response.data[i].current_aum_mgmt)
-                    #response.data[i].current_aum_mgmt = self.OCIPH.cipher_controller(response.data[i].current_aum_mgmt)
+                    response.data[i]['inception_date'] = self.OCIPH.cipher_controller(response.data[i]['inception_date'])
+                if 'current_aum_local' in response.data[i]:
+                    response.data[i]['current_aum_local'] = self.OCIPH.cipher_controller(response.data[i]['current_aum_local'])
+                if 'current_aum_mgmt' in response.data[i]:
+                    response.data[i]['current_aum_mgmt'] = self.OCIPH.cipher_controller(response.data[i]['current_aum_mgmt'])
         return response
 '''
     def getChildItem(self, var):
