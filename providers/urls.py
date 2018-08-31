024541031ebf1a3051850f7219896c6f604e7ade
De Jonckheere Stéphane <humble.jok@gmail.com>
2018-08-31 23:53:47 +0200

    URLS correction

--- providers/urls.py
+++ providers/urls.py
@@ -9,7 +9,7 @@ from providers.views import create_account_from, assign_account_to,\
     accept_external_transaction
 
 urlpatterns = [
-    url('account/(?P<account_holding_id>.+)/portfolio/(?P<holdings_id>.+)$', create_account_from),
-    url('account/(?P<account_holding_id>.+)/assign/(?P<account_id>.+)$', assign_account_to),
-    url('transaction/accept/(?P<external_transaction_id>.+)$', accept_external_transaction),
+    url('account/(?P<account_holding_id>.+)/portfolio/(?P<holdings_id>.+)/$', create_account_from),
+    url('account/(?P<account_holding_id>.+)/assign/(?P<account_id>.+)/$', assign_account_to),
+    url('transaction/accept/(?P<external_transaction_id>.+)/$', accept_external_transaction),
     ]
